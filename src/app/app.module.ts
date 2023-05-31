f2ef829d7b3961ccdb90e9438c242d549cc496e7
unknown <sanakhan23november@gmail.com>
2023-05-31 15:28:15 +0530

    push2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ComponenetComponent } from './componenet/componenet.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ComponenetComponent
+    ComponenetComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
